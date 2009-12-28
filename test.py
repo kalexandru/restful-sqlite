8d2d3485d17ace06ebbae73a8fc15d80dce03508
Charles Hooper <chooper@plumata.com>
2009-12-27 19:02:56 -0500

    General cleanup: Rename of variables, removed unused import. Tests still OK

--- test.py
+++ test.py
@@ -7,92 +7,93 @@ import unittest
 
 from os import unlink,mkdir,removedirs,path,access,F_OK
 from random import randint
-import sqlite3
 
-TESTDIR = '/tmp/testdb%d' % randint(0,99)
-TESTDB = 'test'
-TESTTBL = 'testtbl'
-TESTTBLSQL = """CREATE TABLE `%s` (col1 INT, col2 TEXT)""" % TESTTBL
+TEST_DIR = '/tmp/testdb%d' % randint(0,99)
+TEST_DB = 'test'
+TEST_TABLE = 'testtbl'
+TEST_TABLESQL = """CREATE TABLE `%s` (col1 INT, col2 TEXT)""" % TEST_TABLE
 
 class TestDBConnect(unittest.TestCase):
     def setUp(self):
-        mkdir(TESTDIR)
+        mkdir(TEST_DIR)
 
     def testconnectnocreate(self):
-        self.assertRaises(db.NoSuchDatabase, db._connect, TESTDB, False)
+        self.assertRaises(db.NoSuchDatabase, db._connect, TEST_DB, False)
 
     def testconnectwithcreate(self):
-        conn = db._connect(TESTDB, True)
+        conn = db._connect(TEST_DB, True)
         self.assertNotEqual(conn, None, '_connect returned None')
         conn.close()
 
     def tearDown(self):
-        if access(TESTDB,F_OK):
-            unlink(TESTDB) 
+        if access(TEST_DB,F_OK):
+            unlink(TEST_DB) 
 
-        if access(TESTDIR,F_OK):
-            removedirs(TESTDIR)
+        if access(TEST_DIR,F_OK):
+            removedirs(TEST_DIR)
 
 
-class TestDBMainFuncs(unittest.TestCase):
+class TestDBReadFuncs(unittest.TestCase):
     def setUp(self):
-        mkdir(TESTDIR)
-        db.settings.data_path = TESTDIR
-        self.conn = db._connect(path.join(TESTDIR,TESTDB),True)
+        mkdir(TEST_DIR)
+        db.settings.data_path = TEST_DIR
+        self.conn = db._connect(path.join(TEST_DIR,TEST_DB),True)
 
     def test_list_databases(self):
         res = db.list_databases()
-        self.assertEqual(res, [TESTDB],
-            'list_databases should only include TESTDB')
+        self.assertEqual(res, [TEST_DB],
+            'list_databases should only include TEST_DB')
 
     def test_list_tables(self):
-        res = db.list_tables(TESTDB)
+        res = db.list_tables(TEST_DB)
         self.assertEqual(res, [],
             'list_tables should be empty at this stage')
 
-        self.conn.execute(TESTTBLSQL)
-        res = db.list_tables(TESTDB)
-        self.assertEqual(res, [TESTTBL],
-            'list_tables should only include TESTTBL at this stage')
+        self.conn.execute(TEST_TABLESQL)
+        res = db.list_tables(TEST_DB)
+        self.assertEqual(res, [TEST_TABLE],
+            'list_tables should only include TEST_TABLE at this stage')
 
     def test_all_records(self):
-        self.conn.execute(TESTTBLSQL)
-        res = [rec for rec in db.all_records(TESTDB,TESTTBL)]
+        self.conn.execute(TEST_TABLESQL)
+        res = [rec for rec in db.all_records(TEST_DB,TEST_TABLE)]
         self.assertEqual(res, [],
             'all_records should return an empty set at this stage')
 
-        set = []
+        expected_set = []
         for i in range(10):
             num = randint(0,99)
-            set.append( (num, 'TEST%d' % num) )
-            self.conn.execute("""INSERT INTO `%s` VALUES (?, ?)""" % TESTTBL,
+            expected_set.append( (num, 'TEST%d' % num) )
+            self.conn.execute("""INSERT INTO `%s` VALUES (?, ?)""" % TEST_TABLE,
                 (num, 'TEST%d' % num))
 
-        for record in db.all_records(TESTDB, TESTTBL):
-            self.assert_(record in set)
+        for record in db.all_records(TEST_DB, TEST_TABLE):
+            self.assert_(record in expected_set)
 
     def test_get_record(self):
-        self.conn.execute(TESTTBLSQL)
-        res = db.get_record(TESTDB,TESTTBL,1)
+        self.conn.execute(TEST_TABLESQL)
+        res = db.get_record(TEST_DB,TEST_TABLE,1)
         self.assertEqual(res, None)
 
         num = randint(0,99)
         expected_result = (1, num, 'TEST%d' % num)
         self.conn.execute("""INSERT INTO `%s` (ROWID,col1,col2)
-            VALUES (1,?,?)""" % TESTTBL, (num, 'TEST%d' % num))
+            VALUES (1,?,?)""" % TEST_TABLE, (num, 'TEST%d' % num))
         self.conn.commit()
-        res = db.get_record(TESTDB,TESTTBL,1)
+        res = db.get_record(TEST_DB,TEST_TABLE,1)
         self.assertEqual(res, expected_result,
-            'get_record did not return expected result. expected: %s. got: %s' % (repr(expected_result), repr(res)))
+            '''get_record did not return expected result.
+                expected: %s
+                got: %s''' % (repr(expected_result), repr(res)))
 
         
     def tearDown(self):
         self.conn.close()
-        if access(path.join(TESTDIR,TESTDB),F_OK):
-            unlink(path.join(TESTDIR,TESTDB))
+        if access(path.join(TEST_DIR,TEST_DB),F_OK):
+            unlink(path.join(TEST_DIR,TEST_DB))
 
-        if access(TESTDIR,F_OK):
-            removedirs(TESTDIR)
+        if access(TEST_DIR,F_OK):
+            removedirs(TEST_DIR)
 
 
 if __name__ == '__main__':
